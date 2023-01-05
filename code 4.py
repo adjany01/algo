5e68f3e6ab855e604cbf8af4808c7568c5baf958
Mwambacherubin1710 <122030175+Mwambacherubin1710@users.noreply.github.com>
2023-01-05 15:21:33 +0100

    PROJET V 1 MWAMBA NZAMBI

---
+++ code 4.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+
+from abc import ABCMeta, abstractmethod
+from math import pi, sqrt
+class Geo_Form(metaclass = ABCMeta):
+    @abstractmethod
+    def perimetre():
+        pass
+
+    @abstractmethod
+    def surface():
+        pass
+        
+class Rectangle(Geo_Form):
+    try:
+        def __init__(self,nomF, longueur, largeur):
+            self.nomF = nomF
+            self.longueur = longueur
+            self.largeur = largeur
+        def perimetre(self):
+            return 2*self.longueur + 2*self.largeur
+        
+        def surface(self):
+            return self.longueur*self.largeur
+    except:
+        print("Données non pris en charge")
+
+class Cercle(Geo_Form):  
+    try:
+        def __init__(self, nomF, rayon):
+            self.nomF = nomF
+            self.rayon = rayon
+        def perimetre(self):
+            return 2*pi*self.rayon
+        def surface(self):
+            return  pi*(self.rayon**2)
+    except:
+        print("Données non pris en charge")
+
+class Triangle(Geo_Form):
+    try:
+        def __init__(self,nomF, coteA,coteB,coteC):
+            self.nomF = nomF
+            self.coteB = coteB
+            self.coteA = coteA
+            self.coteC = coteC
+        def perimetre(self):
+            return self.coteB + self.coteA + self.coteC
+
+        def surface(self):
+            p = self.perimetre()/2
+            aire = sqrt(p*(p - self.coteA)*(p - self.coteB)*(p - self.coteC))
+            aire = aire.real
+            return aire
+    except:
+        print("Données non pris en charge")
+class Carre(Rectangle):
+    try:
+        def __init__(self,nomF, cote):
+            Rectangle.__init__(self, nomF, cote, cote)
+    except:
+        print("Données non pris en charge ")
+
+class TriangleRectangle(Triangle):
+    try:
+        def __init__(self,nomF, base, hauteur):
+            hyp = sqrt(base**2+hauteur**2)
+            Triangle.__init__(self, nomF, base, hauteur, hyp)
+    except:
+        print("Données non pris en charge ")
+
+class GeoFig():  
+    try:
+        def __init__(self):
+            self.gGeo_rep = []
+        def add(self, fig):
+            self.gGeo_rep.append(fig)
+        
+    except:
+        print("Données non pris en charge")
+
+def tout_perimetre(obj):
+    return obj.perimetre()
+
+def tout_superficie(obj):
+    return obj.surface()
+    
+def decris_toi(obj):
+    print("Je suis {} \nMon perimetre : {} \nMa surface : {}".format(obj.nomF, tout_perimetre(obj) ,tout_superficie(obj)))
